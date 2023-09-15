0bd38691eacdbf11957de70c34b531b096399508
OLUBI JOHN <olubijohn4live@gmail.com>
2023-09-15 07:00:02 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+
+    import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to guarantyresources,</h1>
+      <p>
+These terms and conditions outline the rules and regulations for the use of guarantyresources’s website, located at guarantyresources.com. By accessing this website we assume you accept these terms and conditions. Do not continue to use guarantyresources if you do not agree to take all of the terms and conditions stated on this page.
+
+The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: “Client”, “You” and “Your” refers to you, the person who logs on this website and is compliant with the Company’s terms and conditions. “The Company”, “Ourselves”, “We”, “Our” and “Us”, refers to our Company. “Party”, “Parties”, or “Us”, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of the provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to the same.
+
+Cookies
+We employ the use of cookies. By accessing guarantyresources, you agreed to use cookies in agreement with the guarantyresources Privacy Policy.
+Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
+
+License
+Unless otherwise stated, guarantyresources own the intellectual property rights for all material on guarantyresources. All intellectual property rights are reserved. You may access this from guarantyresources.com for your own personal use subjected to restrictions set in these terms and conditions.
+You are not allowed to:
+<ul>
+  <li>Republish material from guarantyresources</li>
+  <li>Sell, rent or sub-license material from guarantyresources</li>
+  <li>Reproduce, duplicate or copy material from guarantyresources</li>
+  <li>Redistribute content from guarantyresources</li>
+</ul>
+
+This Agreement shall begin on the date hereof
+
+Parts of this website may offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Guarantyresources does not filter, edit, publish or review comments prior to their presence on the website. Comments do not reflect the views and opinions of guarantyresources, its agents and/or affiliates. Comments reflect the views and opinions of the persons who post such view and/or opinion. To the extent permitted by applicable laws, guarantyresources shall not be liable for the comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the comments on this website.
+
+Guarantyresources reserves the right to monitor all comments and to remove any comments which can be considered inappropriate, offensive or causes a breach of these terms and conditions.
+You warrant and represent that:
+<ul>
+<li> 	You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
+<li> The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+<li> The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy</li>
+<li> The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+<li> You hereby grant guarantyresources a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your comments in any and all forms, formats or media.</li>
+</ul>
+
+Hyperlinking to our Content
+The following organizations may link to our Website without prior written approval:
+<ul>
+<li>	Government agencies;</li>
+<li>	Search engines;</li>
+<li>	News organizations;</li>
+</ul>
+
+Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and
+System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups that may not hyperlink to our Web site.
+
+These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.
+We may consider and approve other link requests from the following types of organizations:
+<ul> 
+<li>	Commonly-known consumer and/or business information sources;</li>
+<li>	dot.com community sites;</li>
+<li>	associations or other groups representing charities;</li>
+<li>	online directory distributors;</li>
+<li>	internet portals;</li>
+<li>	accounting, law and consulting firms; and</li>
+<li>	educational institutions and trade associations.</li>
+</ul>
+
+We will approve link requests from these organizations at our discretion 
+
+iFrames
+
+Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.
+
+Content Liability
+We shall not be held responsible for any content that appears on your Website. You agree to protect and defend us against all claims that are rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
+
+Removal of links from our website
+If you find any link on our Website that is offensive for any reason, you are free to contact and inform us at any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.
+
+We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.
+
+Disclaimer
+<li>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</li>
+<li>limit or exclude our or your liability for death or personal injury;</li>
+<li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+<li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+<li>Exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+
+</p>
     </div>
   );
 }
